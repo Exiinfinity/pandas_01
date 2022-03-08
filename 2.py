b0f87e12686139aedf1fcf70df9268e7533c8b86
infinity <88617484+Exiinfinity@users.noreply.github.com>
2022-03-08 17:39:46 +0900

    day1

---
+++ 2.py
@@ -0,0 +1,13 @@
+# for i in range(1,10):
+#     for k in range(0,10,2):
+#         print("파이썬 정말 재미있어요~")
+# print("%d"%(500+500))
+# print("%d"%(500,600))
+# print("%d%d"%(700,a))
+# inStr,outStr = 'Python',""
+# strLen = len(inStr)
+# for i in range(0,strLen):
+#     outStr += inStr[strLen-i]
+s="universe"
+print(s[2])
+print(s[-2])
--- variable1.py
+++ variable1.py
@@ -1,4 +1,4 @@
-# 2021-12-20 변수
+
 # 1) 레퍼런스 : 값(객체)이 저장된 메모리의 위치를 가리키는
 #     레퍼런스(reference : 참조)
 # 2) 동적 타이핑 : 값의 형에 따라서 고정되지 않고,
@@ -75,15 +75,15 @@ del z
 # print()를 이용하여 한 줄에 출력하시오.
 # 이름은 문자열로, 나이는 숫자로 저장
 
-name = '이경미'
+name = 'kim'
 age = 20
 
-# name, age = '이경미', 20
+# name, age = 'kim', 20
 # print(name + ' ' +str(age))
 # print('나는 ' + name + ' 나이는 ' +str(age))
 print(name, age)
 
-name='이성길'
+name='lee'
 age=27
 print('이름:{},나이:{}'.format(name, age))
 print('성명 : %s, 나이 : %d' %(name, age))
@@ -124,11 +124,10 @@ print('평균 :' ,average)
 # 형식 : print('문자열 %d 문자열' % 변수)
 # 서식 : %d %f %s %o %x %%
 
-name='이성길'
+name='lee'
 age=27
 print('나이 : %d살' % age)
 print('이름 : %s' % name)
-
 
 name, no, year, grade, average = '이유진', 1234, 4, 'A+', 100
 print('성명 : %s\n학번 : %d\n학년 : %d\n학점 : %s\n평균 : %d' %(name ,no,year,grade,average))
